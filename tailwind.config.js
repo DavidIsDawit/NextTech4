a67001a55e42fad2b53a18e3881dcf7fd483b905
Abdii Biranu <abdibiranugaenginering.et>
2026-01-21 17:17:42 +0300

    l

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: "Roboto Mono",
+      sans: ["Source Sans 3", "sans-serif"],
     },
 
     extend: {
